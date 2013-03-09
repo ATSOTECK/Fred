78370c18e9e9953641e0f4917cd9c7b3f6ffc411
Skyler Burwell <skyboy49707@gmail.com>
2013-03-09 00:29:05 -0800

    Added a splash screen.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPixmap pixmap(":/Images/loading.png");
+    QSplashScreen splash(pixmap);
+    splash.setWindowFlags(Qt::WindowStaysOnTopHint | Qt::SplashScreen);
+    splash.show();
+    splash.showMessage("Getting cameras...");
+    a.processEvents();
     MainWindow w;
     w.showMaximized();
-    
+    splash.finish(&w);
+
     return a.exec();
 }
